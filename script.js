7026e643636515687789778e932c5a8737b3a902
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-08-16 18:14:06 +0100

    Add code
    Adding wining game features

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ const diceEl = document.querySelector(`.dice`);
 const btnNew = document.querySelector(`.btn--new`);
 const btnRoll = document.querySelector(`.btn--roll`);
 const btnHold = document.querySelector(`.btn--hold`);
+let gameOver = false;
+
+function switchPlayer() {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle(`player--active`);
+  player1El.classList.toggle(`player--active`);
+}
 
 //Starting conditions
 score0El.textContent = 0;
@@ -20,28 +30,56 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 //Rolling dice functionality;
+
 btnRoll.addEventListener(`click`, function () {
-  //Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  if (!gameOver) {
+    //Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
-  //Display dice
-  diceEl.classList.remove(`hidden`);
-  diceEl.src = `dice-${dice}.png`;
+    //Display dice
+    diceEl.classList.remove(`hidden`);
+    diceEl.src = `dice-${dice}.png`;
 
-  //Check for rolled 1: if true,switch to next player
-  if (dice !== 1) {
-    //Add dice to current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    //Switch next player
-    currentScore = 0;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle(`player--active`);
-    player1El.classList.toggle(`player--active`);
+    //Check for rolled 1: if true,switch to next player
+    if (dice !== 1) {
+      //Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //Switch next player
+      // currentScore = 0;
+      // document.getElementById(`current--${activePlayer}`).textContent =
+      //   currentScore;
+      // activePlayer = activePlayer === 0 ? 1 : 0;
+      // player0El.classList.toggle(`player--active`);
+      // player1El.classList.toggle(`player--active`);
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener(`click`, function () {
+  if (!gameOver) {
+    //Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    //Chek if socre is >=100
+    if (scores[activePlayer] >= 20) {
+      //Finish game
+      gameOver = true;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add(`player--winner`);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove(`player--active`);
+    } //Switch to next player
+    else {
+      switchPlayer();
+    }
   }
 });
